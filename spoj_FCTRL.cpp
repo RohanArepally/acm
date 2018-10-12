2f4d1bbb62043a5b34cce311ff028448505aee50
Rohan Arepally <rarepall@uber.com>
2018-10-11 19:03:23 -0700

    add comment

--- spoj_FCTRL.cpp
+++ spoj_FCTRL.cpp
@@ -6,6 +6,8 @@ int trailing(int n) {
     int sum = 0;
     while(n) {
         sum += (n / 5);
+        // need to divide because you need to count cases where numbercs can have multiple
+        // factors of 5. 25 is 5^2 and 125 is 5^3.
         n /= 5;
     }
     return sum;
